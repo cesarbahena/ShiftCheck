7923f148bc1e482202e4350d384e899c091696a9
cesarbahena <cabahenac@gmail.com>
2022-05-12 14:52:41 -0600

    docs: add XML documentation to services

--- ShiftCheck/Services/IAuthService.cs
+++ ShiftCheck/Services/IAuthService.cs
@@ -2,11 +2,33 @@ using ShiftCheck.Models;
 
 namespace ShiftCheck.Services;
 
+/// <summary>
+/// Service for managing user authentication and authorization
+/// </summary>
 public interface IAuthService
 {
+	/// <summary>
+	/// Authenticates a user with username and password
+	/// </summary>
 	Task<LoginResponse?> LoginAsync(string username, string password);
+
+	/// <summary>
+	/// Logs out the current user and clears stored credentials
+	/// </summary>
 	Task LogoutAsync();
+
+	/// <summary>
+	/// Retrieves the current authentication token
+	/// </summary>
 	Task<string?> GetTokenAsync();
+
+	/// <summary>
+	/// Checks if a user is currently authenticated
+	/// </summary>
 	Task<bool> IsAuthenticatedAsync();
+
+	/// <summary>
+	/// Gets the currently authenticated user information
+	/// </summary>
 	Task<UserDto?> GetCurrentUserAsync();
 }
